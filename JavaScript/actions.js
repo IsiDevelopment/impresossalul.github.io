1cbdcda85e191252d6719e561016db5ba69beb05
isidro <isidro.cano.mtz@outlook.es>
2019-03-24 06:55:44 -0600

    version2

--- JavaScript/actions.js
+++ JavaScript/actions.js
@@ -1,22 +1,100 @@
-  function appear() {
-    $('#label').show()
-  }
+$(function(){
+  $(".formatoShowButton").click(function(){
+    $(".formatoHidden").fadeIn(1000)
+    $(".formatoShowed").hide()
+  })
+  $(".formatoHideButton").click(function(){
+    $(".formatoHidden").hide()
+    $(".formatoShowed").fadeIn(1000)
+  })
 
-  function hideL() {
-    $('#label').hide()
-  }
+  $(".reporteShowButton").click(function(){
+    $(".reporteHidden").fadeIn(1000)
+    $(".reporteShowed").hide()
+  })
+  $(".reporteHideButton").click(function(){
+    $(".reporteHidden").hide()
+    $(".reporteShowed").fadeIn(1000)
+  })
 
-  function h() {
-    var values = "Nuevo valor"
-    $('#c').val('<%= s%>' + ' Vl')
-    $('#label').val('Nuevo Valor')
-  }
+  $(".comandaShowButton").click(function(){
+    $(".comandaHidden").fadeIn(1000)
+    $(".comandaShowed").hide()
+  })
+  $(".comandaHideButton").click(function(){
+    $(".comandaHidden").hide()
+    $(".comandaShowed").fadeIn(1000)
+  })
 
-  function myFunction() {
-      var x = $("#myTopnav");
-      if (x.className === "topnav") {
-          x.className += " responsive";
-      } else {
-          x.className = "topnav";
-      }
-  }
+  $(".solicitudShowButton").click(function(){
+    $(".solicitudHidden").fadeIn(1000)
+    $(".solicitudShowed").hide()
+  })
+  $(".solicitudHideButton").click(function(){
+    $(".solicitudHidden").hide()
+    $(".solicitudShowed").fadeIn(1000)
+  })
+
+  $(".notaShowButton").click(function(){
+    $(".notaHidden").fadeIn(1000)
+    $(".notaShowed").hide()
+  })
+  $(".notaHideButton").click(function(){
+    $(".notaHidden").hide()
+    $(".notaShowed").fadeIn(1000)
+  })
+
+  $(".reciboShowButton").click(function(){
+    $(".reciboHidden").fadeIn(1000)
+    $(".reciboShowed").hide()
+  })
+  $(".reciboHideButton").click(function(){
+    $(".reciboHidden").hide()
+    $(".reciboShowed").fadeIn(1000)
+  })
+
+  $(".remisionShowButton").click(function(){
+    $(".remisionHidden").fadeIn(1000)
+    $(".remisionShowed").hide()
+  })
+  $(".remisionHideButton").click(function(){
+    $(".remisionHidden").hide()
+    $(".remisionShowed").fadeIn(1000)
+  })
+
+  $(".tarjetaShowButton").click(function(){
+    $(".tarjetaHidden").fadeIn(1000)
+    $(".tarjetaShowed").hide()
+  })
+  $(".tarjetaHideButton").click(function(){
+    $(".tarjetaHidden").hide()
+    $(".tarjetaShowed").fadeIn(1000)
+  })
+
+  $(".hojaShowButton").click(function(){
+    $(".hojaHidden").fadeIn(1000)
+    $(".hojaShowed").hide()
+  })
+  $(".hojaHideButton").click(function(){
+    $(".hojaHidden").hide()
+    $(".hojaShowed").fadeIn(1000)
+  })
+
+  $(".sobresShowButton").click(function(){
+    $(".sobresHidden").fadeIn(1000)
+    $(".sobresShowed").hide()
+  })
+  $(".sobresHideButton").click(function(){
+    $(".sobresHidden").hide()
+    $(".sobresShowed").fadeIn(1000)
+  })
+
+  $(".carpetaShowButton").click(function(){
+    $(".carpetaHidden").fadeIn(1000)
+    $(".carpetaShowed").hide()
+  })
+  $(".carpetaHideButton").click(function(){
+    $(".carpetaHidden").hide()
+    $(".carpetaShowed").fadeIn(1000)
+  })
+})
--- JavaScript/owl_carousel.js
+++ JavaScript/owl_carousel.js
@@ -1,18 +1,10 @@
 var $pap_interna = $('#pap_interna');
 var $pap_empresarial = $('#pap_empresarial');
-var $beer = $('#beer');
-var $tabacco = $('#tabacco');
 
 $pap_interna.children().each( function( index ) {
   $(this).attr( 'data-position', index );
 });
 $pap_empresarial.children().each( function( index ) {
-  $(this).attr( 'data-position', index );
-});
-$beer.children().each( function( index ) {
-  $(this).attr( 'data-position', index );
-});
-$tabacco.children().each( function( index ) {
   $(this).attr( 'data-position', index );
 });
 
@@ -26,26 +18,10 @@ $pap_empresarial.owlCarousel({
   loop: true,
   items: 3
 });
-$beer.owlCarousel({
-  center: true,
-  loop: true,
-  items: 3
-});
-$tabacco.owlCarousel({
-  center: true,
-  loop: true,
-  items: 3
-});
 
 $($pap_interna).on('click', '.owl-item>div', function() {
   $pap_interna.trigger('to.owl.carousel', $(this).data( 'position' ) );
 });
 $($pap_empresarial).on('click', '.owl-item>div', function() {
   $pap_empresarial.trigger('to.owl.carousel', $(this).data( 'position' ) );
-});
-$($beer).on('click', '.owl-item>div', function() {
-  $beer.trigger('to.owl.carousel', $(this).data( 'position' ) );
-});
-$($tabacco).on('click', '.owl-item>div', function() {
-  $tabacco.trigger('to.owl.carousel', $(this).data( 'position' ) );
 });
